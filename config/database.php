fd31d2f030aca60ea45a06f5029140d5609d7b8e
Kyrill <kyrilldumerchat@gmail.com>
2023-10-25 10:44:01 +0200

    test connection bdd v2

--- config/database.php
+++ config/database.php
@@ -64,8 +64,8 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'driver' => 'pgsql',
+            'url' => 'site-sae-ubisoufte.bigpapoo.com',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'alhaiz_breizh'),
