b20677fe6f1d76843dbbbc412059fa917f492ea0
Paitaz <136081479+MathisLB@users.noreply.github.com>
2023-10-25 12:10:31 +0200

    Rajout du code de romain pour l'appartition d'un message
    JPP

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,3 +1,5 @@
+var devisCree = false; // Initialisez-le à false au départ
+
 document.getElementById("genererPDF").addEventListener("click", function () {
 
 
@@ -12,5 +14,62 @@ document.getElementById("genererPDF").addEventListener("click", function () {
 
     // Utilisation de html2pdf.js pour générer le PDF avec le formulaire
     const form = document.getElementById("myForm");
-    html2pdf().set(options).from(form).save();
+    devisCree = true;
+    html2pdf().set(options).from(form).save().then(function (pdf) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "update_devis.php", true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.send("devisCree=true");
+        
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Réponse du serveur reçue
+                // Vous pouvez effectuer d'autres actions ici, par exemple, afficher un message.
+                console.log("PDF généré avec succès");
+            }
+        };
+    });
 });
+
+
+
+/*
+document.getElementById("genererPDF").addEventListener("click", function () {
+    const options = {
+        margin: [0, 1],
+        filename: 'Mon_Devis.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    const form = document.getElementById("myForm");
+    
+    // Utilisation de html2pdf.js pour générer le PDF
+    html2pdf().set(options).from(form).outputPdf(pdf => {
+        // Créez un objet Blob contenant le PDF
+        const blob = new Blob([pdf], { type: 'application/pdf' });
+
+        // Déterminez l'emplacement de stockage de votre choix
+        const storageLocation = '/home/etuinfo/rjacovetti/Documents/IUT/Semestre3/SAE/DevisToPDF/';
+
+        // Créez un objet URL pour le Blob
+        const url = URL.createObjectURL(blob);
+
+        // Créez un lien de téléchargement avec le PDF
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'Mon_Devis.pdf';
+
+        // Placez le lien de téléchargement dans votre emplacement de stockage
+        a.style.display = 'none';
+        document.body.appendChild(a);
+
+        // Déclenchez le téléchargement
+        a.click();
+
+        // Libérez la ressource URL
+        window.URL.revokeObjectURL(url);
+    });
+});
+*/
