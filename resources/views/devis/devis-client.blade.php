c701ba63592505ee54837f3c565b2500a50a72a5
Paitaz <136081479+MathisLB@users.noreply.github.com>
2023-10-25 11:33:39 +0200

    Update devis-client.blade.php
    passage en laravel

--- resources/views/devis/devis-client.blade.php
+++ resources/views/devis/devis-client.blade.php
@@ -2,56 +2,42 @@
 <html lang="fr">
 
 <head>
-
-  <meta charset="utf-8" />
-  <title>Demande de devis</title>
-  <meta name="Devis" content=""/>
-  <meta name="keywords" content="AlHaizBreizh"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" sizes="16x16" href="./assets/IMG/logo.png">
-  <link rel="stylesheet" href="./assets/CSS/style-devis.css">
-
+    <meta charset="utf-8" />
+    <title>Demande de devis</title>
+    <meta name="Devis" content="" />
+    <meta name="keywords" content="AlHaizBreizh" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/IMG/logo.png') }}">
+    <link rel="stylesheet" href="{{ asset('assets/CSS/style-devis.css') }}">
 </head>
 
-
 <body>
-        <header>
-            <img src="./assets/IMG/image_header.png">
-            <hr>
-        </header>
+    <header>
+        <img src="{{ asset('assets/IMG/image_header.png') }}">
+        <hr>
+    </header>
+
     <main>
         <h1>Demander le devis :</h1>
-        $devisCree = file_get_contents("devis_state.txt");
-        <form action='devis-suite.php' method='POST' name="devis">
-        <section>
-            <div>    
-                <section class="p1">
-                    <button type="button">Demander devis</button>
+        @if ($devisCree === 'true')
+            <form action="{{ route('accept-devis') }}" method="POST">
+                @csrf
+                <section>
+                    <div>
+                        <section class="p1">
+                            <button type="submit">Demander devis</button>
+                        </section>
+                        <section class="p3">
+                            <p>Accepter ou Refuser le devis :</p>
+                            <button type="submit" name="accepter" value="true">Accepter devis</button>
+                            <button type="submit" name="refuser" value="true">Refuser devis</button>
+                        </section>
+                    </div>
                 </section>
-                <section class="p3">
-                    @if ($devisCree === 'true')
-                        <p>Accepter ou Refuser le devis :</p>
-                        <button type="button">Accepter devis</button>
-                        <button type="button">Refuser devis</button>
-                    <p>Télécharger PDF</p>
-                    <a href="Mon_Devis.pdf" download="Mon_Devis.pdf">Télécharger le PDF</a>
-                </section>
-            </div>
-        </form>
+            </form>
+        @endif
+        <p>Télécharger PDF</p>
+        <a href="{{ route('telecharger-pdf') }}" download="Mon_Devis.pdf">Télécharger le PDF</a>
     </main>
 </body>
-<script>
-document.getElementById("refuserDevis").addEventListener("click", function () {
-    // Effectuez ici toute action nécessaire, par exemple, enregistrez le refus du devis dans la base de données.
-    // Redirigez ensuite l'utilisateur vers proprio.php avec un message.
-    window.location.href = 'proprio.php?refus=1';
-});
-</script>
-<script>
-document.getElementById("accepterDevis").addEventListener("click", function () {
-    // Effectuez ici toute action nécessaire, par exemple, enregistrez le refus du devis dans la base de données.
-    // Redirigez ensuite l'utilisateur vers proprio.php avec un message.
-    window.location.href = 'proprio.php?accept=1';
-});
-</script>
 </html>
