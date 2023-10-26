810711c33a814f656d986a9dcd52739d21ca5878
EwanP56 <120094397+EwanP56@users.noreply.github.com>
2023-10-26 09:21:09 +0200

    Update page_paiement.blade.php

--- resources/views/page_paiement.blade.php
+++ resources/views/page_paiement.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet"  href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/css/bootstrap-datepicker.min.css">
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}" />
     <title>Paiement</title>
 </head>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
@@ -15,7 +15,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="script.js"></script>
     <header>
-        @include('components.navbar')
+        <x-Navbar></x-Navbar>
     </header>
 
     <h1>Procédure de paiement :</h1>
@@ -26,18 +26,19 @@
         <section>
         <form action="https://www.caisse-epargne.fr">
             <div class="mb-3">
-               <li>Nom du propriétaire de la carte : </li>
+                <img class="image_nom" src="{{asset('img/name.png')}}">
+                <li>Nom du propriétaire de la carte : </li>
                 <input type="nom" class="form-control" placeholder="Ex : Jean DUPONT">
             </div>
             <div class="mb-3">
-                <img src="img/card.png">
+                <img src="{{asset('img/card.png')}}">
                 <li>Numéro de la carte : </li>
                  <input type="nom" class="form-control" placeholder="Ex : 1234-5678-9012-3456">
              </div>
                 <div class="datepicker date input-group">
                     <div class="input-group-append">
                         <li>
-                            <img src="img/calendar.png">
+                            <img src="{{asset('img/calendar.png')}}">
                             Date :
                             <i class="fa fa-calendar"></i>
                         </li>
@@ -45,7 +46,7 @@
                     <input type="text" placeholder="Choisir une date" class="form-control" id="reservationDate">
                 </div>
              <div class="mb-3">
-                <img src="img/lock.png">
+                <img src="{{asset('img/lock.png')}}">
                 <li>Code de sécurité : </li>
                  <input type="nom" class="form-control" placeholder="Ex : ">
              </div>
